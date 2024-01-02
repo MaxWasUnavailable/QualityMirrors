8d258b54ac6005a2965b6a6fe8ebdaca2c7d32ce
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-02 18:58:24 +0100

    Cleanup

--- QualityMirrors/Patches/MirrorReflectionPatches.cs
+++ QualityMirrors/Patches/MirrorReflectionPatches.cs
@@ -12,13 +12,13 @@ internal static class MirrorReflectionPatches
     private static IEnumerable<CodeInstruction> IncreaseMirrorResolutions(IEnumerable<CodeInstruction> instructions)
     {
         return new CodeMatcher(instructions)
-            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int) instruction.operand == 128)
+            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 128)
             .SetOperandAndAdvance(512)
-            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int) instruction.operand == 256)
+            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 256)
             .SetOperandAndAdvance(1024)
-            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int) instruction.operand == 512)
+            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 512)
             .SetOperandAndAdvance(2048)
-            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int) instruction.operand == 1024)
+            .SearchForward(instruction => instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 1024)
             .SetOperandAndAdvance(4096)
             .InstructionEnumeration();
     }
